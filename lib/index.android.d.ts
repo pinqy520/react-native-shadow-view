1b783a66d0a002bba3d97fb8a425c6c40d96e5f4
黄祺 <pinqy520@gmail.com>
2017-05-26 11:26:40 +0800

    move to lib dir

---
+++ lib/index.android.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="react" />
+import * as React from 'react';
+import { ViewProperties } from 'react-native';
+export declare class ShadowView extends React.Component<ViewProperties, any> {
+    render(): JSX.Element;
+}
+declare const _default: typeof ShadowView;
+export default _default;
