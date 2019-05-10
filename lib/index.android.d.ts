f205aecef124f1073bc2d4f8895a6d80e709fe1e
Kyle Fang <zhigang1992@gmail.com>
2019-05-10 12:56:21 +0800

    chore: clean up

--- lib/index.android.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-import * as React from 'react';
-import { ViewProperties } from 'react-native';
-export declare class ShadowView extends React.Component<ViewProperties, any> {
-    render(): JSX.Element;
-}
-export default ShadowView;
